970b5df9824c51593492f5e13b8b1fae1157d917
tim <tim@keynes.fr>
2023-09-02 18:00:54 +0200

    V1 version of new doc

--- gen_swagger.sh
+++ gen_swagger.sh
@@ -1,3 +1,3 @@
 # Node v14.21.3
 
-npx @mintlify/scraping@latest openapi-file ./swagger.json -o api-reference --overwrite
+npx @mintlify/scraping@latest openapi-file ./swagger.json -o api-v2-reference --overwrite
