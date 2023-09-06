f88153f3de30641a945dcfd753e6df550c79ec81
tim <tim@keynes.fr>
2023-09-06 20:37:05 +0200

    Swagger v2.0 to v3.0

--- gen_swagger_geography.sh
+++ gen_swagger_geography.sh
@@ -1,4 +1,15 @@
 # Node v14.21.3
 
-curl https://elasticimmo.prod.services.f6o.io/swagger/geography/geography.swagger.json --output swagger-geography.json
-sudo npx @mintlify/scraping@latest openapi-file ./swagger-geography.json -o api-geography-reference --overwrite
+#swagger="$(curl -X 'GET' 'https://converter.swagger.io/api/convert?url=https%3A%2F%2Felasticimmo.prod.services.f6o.io%2Fswagger%2Fgeography%2Fgeography.swagger.json' -H 'accept: application/json')"
+
+swagger=$(echo "$(curl -X 'GET' 'https://converter.swagger.io/api/convert?url=https%3A%2F%2Felasticimmo.prod.services.f6o.io%2Fswagger%2Fgeography%2Fgeography.swagger.json' -H 'accept: application/json')" | sed 's|\"/\"|\"https://elasticimmo-doc.prod.services.f6o.io/\"|g')
+echo $swagger > swagger-geography.json
+
+#https://elasticimmo-doc.prod.services.f6o.io/
+
+
+
+#echo ${$swagger/"/"/https://elasticimmo.prod.services.f6o.io}
+#   --output swagger-geography.json
+#curl https://elasticimmo.prod.services.f6o.io/swagger/geography/geography.swagger.json --output swagger-geography.json
+#sudo npx @mintlify/scraping@latest openapi-file ./swagger-geography.json -o api-geography-reference --overwrite
