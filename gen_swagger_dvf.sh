8eb2bc912d19f2bd2b9c7c82d4ab2a8aa03687dd
FrancoisChastel <francois@chastel.co>
2023-10-12 12:34:59 +0200

    Add - DVF documentation

--- gen_swagger_dvf.sh
+++ gen_swagger_dvf.sh
@@ -1,6 +1,6 @@
 swagger=$(echo "$(curl -X 'GET' 'https://converter.swagger.io/api/convert?url=https%3A%2F%2Felasticimmo.prod.services.f6o.io%2Fswagger%2Fdvf%2Fdvf.swagger.json' -H 'accept: application/json')" | sed 's|\"/\"|\"https://elasticimmo-doc.prod.services.f6o.io"|g')
 echo $swagger > swagger-dvf.json
-npx @mintlify/scraping@3.0.33 openapi-file ./swagger-dvf.json -o api-dvf-reference
+npx @mintlify/scraping@3.0.33 openapi-file ./swagger-dvf.json -o api-experimental-reference
 #https://elasticimmo-doc.prod.services.f6o.io/
 
 
