18d468c334de010c36b770897152fcc0873be35a
tim <tim@keynes.fr>
2023-10-16 16:20:35 +0200

    Cleaning

--- gen_swagger_api_v2.sh
+++ gen_swagger_api_v2.sh
@@ -1,4 +1,4 @@
 # Node v14.21.3
 
 curl https://api.fluximmo.io/swagger --output swagger-api-v2.json
-npx @mintlify/scraping@latest openapi-file ./swagger-api-v2.json -o api-v2-reference --overwritenpx @mintlify/scraping@latest openapi-file ./swagger-api-v2.json -o api-v2-reference --overwrite
+npx @mintlify/scraping@latest openapi-file ./swagger-api-v2.json -o api-v2-reference
--- gen_swagger_experimental.sh
+++ gen_swagger_experimental.sh
@@ -1,7 +1,7 @@
-swagger=$(echo "$(curl -X 'GET' 'https://converter.swagger.io/api/convert?url=https%3A%2F%2Felasticimmo.prod.services.f6o.io%2Fswagger%2Fcity%2Fcity.swagger.json' -H 'accept: application/json')" | sed 's|\"/\"|\"https://elasticimmo-doc.prod.services.f6o.io"|g')
+swagger=$(echo "$(curl -X 'GET' 'https://converter.swagger.io/api/convert?url=https%3A%2F%2Felasticimmo.prod.services.f6o.io%2Fswagger%2Fcity%2Fcity.swagger.json' -H 'accept: application/json')" | sed 's|\"/\"|\"https://elasticimmo.prod.services.f6o.io"|g')
 echo $swagger > swagger-city.json
-npx @mintlify/scraping@3.0.33 openapi-file ./swagger-city.json -o api-experimental-reference
+npx @mintlify/scraping@latest openapi-file ./swagger-city.json -o api-experimental-reference
 
-swagger=$(echo "$(curl -X 'GET' 'https://converter.swagger.io/api/convert?url=https%3A%2F%2Felasticimmo.prod.services.f6o.io%2Fswagger%2Fanalytics%2Fanalytics.swagger.json' -H 'accept: application/json')" | sed 's|\"/\"|\"https://elasticimmo-doc.prod.services.f6o.io"|g')
+swagger=$(echo "$(curl -X 'GET' 'https://converter.swagger.io/api/convert?url=https%3A%2F%2Felasticimmo.prod.services.f6o.io%2Fswagger%2Fanalytics%2Fanalytics.swagger.json' -H 'accept: application/json')" | sed 's|\"/\"|\"https://elasticimmo.prod.services.f6o.io"|g')
 echo $swagger > swagger-analytics.json
-npx @mintlify/scraping@3.0.33 openapi-file ./swagger-analytics.json -o api-experimental-reference
+npx @mintlify/scraping@latest openapi-file ./swagger-analytics.json -o api-experimental-reference
