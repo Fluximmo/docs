e57602cf158e4e1aeb4f14f393c28fb9cca743bc
FrancoisChastel <francois@chastel.co>
2023-11-03 12:40:11 +0100

    Add - New services doc

---
+++ gen_swagger_city.sh
@@ -0,0 +1,11 @@
+swagger=$(echo "$(curl -X 'GET' 'https://converter.swagger.io/api/convert?url=https%3A%2F%2Felasticimmo.prod.services.f6o.io%2Fswagger%2Fcity%2Fcity.swagger.json' -H 'accept: application/json')" | sed 's|\"/\"|\"https://elasticimmo-doc.prod.services.f6o.io"|g')
+echo $swagger > swagger-city.json
+npx @mintlify/scraping@3.0.33 openapi-file ./swagger-city.json -o api-city-reference
+#https://elasticimmo-doc.prod.services.f6o.io/
+
+
+
+#echo ${$swagger/"/"/https://elasticimmo.prod.services.f6o.io}
+#   --output swagger-geography.json
+#curl https://elasticimmo.prod.services.f6o.io/swagger/geography/geography.swagger.json --output swagger-geography.json
+#sudo npx @mintlify/scraping@latest openapi-file ./swagger-geography.json -o api-geography-reference --overwrite
