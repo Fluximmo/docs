8e4a432daf100e58c5895b8e459b2be156c233b8
tim <tim@keynes.fr>
2023-11-07 20:57:01 +0100

    Manually merging branch main

--- gen_swagger_api_v2.sh
+++ gen_swagger_api_v2.sh
@@ -1,4 +1,6 @@
 # Node v14.21.3
 
-curl https://api.fluximmo.io/swagger --output swagger-api-v2.json
-npx @mintlify/scraping@latest openapi-file ./swagger-api-v2.json -o api-v2-reference
+curl https://api.fluximmo.io/swagger | sed 's|x-api-key|x_api_key|g' | sed 's|{"securitySchemes":{"x_api_key":{"type":"apiKey","in":"header","name":"x_api_key"}}|{"securitySchemes":{"x_api_key":{"type":"apiKey","in":"header","name":"x-api-key"}}|g' > swagger-api-v2.json
+#swagger=$(echo "$(curl -X 'GET' 'https://api.fluximmo.io/swagger' -H 'accept: application/json')" | sed 's|x-api-key|x_api_key|g' | sed 's|{"securitySchemes":{"x_api_key":{"type":"apiKey","in":"header","name":"x_api_key"}}|{"securitySchemes":{"x_api_key":{"type":"apiKey","in":"header","name":"x-api-key"}}|g')
+#echo $swagger > swagger-api-v2.json
+sudo npx @mintlify/scraping@latest openapi-file ./swagger-api-v2.json -o api-search-alert-reference --overwrite
